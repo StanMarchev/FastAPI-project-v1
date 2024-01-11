634ebf2beae416e40e7345e8cec66d60388fe47e
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2024-01-11 21:03:11 +0000

    add books table

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+import databases
+import sqlalchemy
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+DATABASE_URL = 'postgresql://postgres:pirata123@localhost:5432/store'
+
+data = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String),
+    sqlalchemy.Column("author",sqlalchemy.String ),
+)

f7db83ed628d4c6db9defd2b5421cfad0ffdf4c9
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2024-01-11 21:09:49 +0000

    create startup and shutdown

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import databases
 import sqlalchemy
+from fastapi import FastAPI
 
 
 DATABASE_URL = 'postgresql://postgres:pirata123@localhost:5432/store'
 
-data = databases.Database(DATABASE_URL)
+database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
@@ -16,3 +17,17 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("author",sqlalchemy.String ),
 )
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all()
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
